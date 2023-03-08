a7536ab7868280bc4485644d5812089fcbf825a2
R <ryansereno@protonmail.com>
2023-03-08 07:27:04 -0500

    removed redundant if check

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 from mutagen.easyid3 import EasyID3
 import os
 
-print("input directory for processing: ")
+print("input directory: ")
 path = input()
 os.chdir(path)
 
@@ -9,13 +9,12 @@ file_list = filter((lambda x: '.mp3' in x), os.listdir(path))
 
 for file in file_list:
         print("file: "+file)
-        if file.endswith(".mp3"): 
-                try:
-                        current = EasyID3(file)
-                        newname = current["title"][0] + ".mp3"
-                        newname.replace(" ", "_")
-                        del current
-                        print("renaming "+file+" to "+newname)
-                        os.rename(file, newname)
-                except:
-                        print("there was an error")
+        try:
+                current = EasyID3(file)
+                newname = current["title"][0] + ".mp3"
+                newname.replace(" ", "_")
+                del current
+                print("renaming "+file+" to "+newname)
+                os.rename(file, newname)
+        except:
+                print("there was an error")
